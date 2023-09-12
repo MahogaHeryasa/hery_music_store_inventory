8b972321b9391b2ffe36e86dbb0e2b1b130c2cf4
MahogaHeryasa <hery.heryasa@gmail.com>
2023-09-12 20:38:25 +0700

    menambahkan context pada views.py

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
+        'application_name': 'Hery Music Store Inventory',
         'name': 'Mahoga Aribowo Heryasa',
         'class': 'PBP D'
     }
